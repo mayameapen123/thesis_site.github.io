55dc3e660b572f1e01628de283c519c13b47be88
mayameape <59280026+mayameape@users.noreply.github.com>
2020-10-27 23:03:37 +0530

    Fourth attempt

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ $("#v0").mouseenter(function(){
 
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    console.log(scroll);
+
     if(scroll<800){
 
     //Food video shows
@@ -70,8 +70,8 @@ window.addEventListener("scroll", (event) => {
        $("#v1").show();
        $("#v1").trigger("play");
        $("#v1").autoplay = true;
-       $("a:first").css("opacity","0.5");
-       $("#a2").css("opacity","1");
+      
+      //$("#a2").css("opacity","1");
     }
 
     //Section one shows
@@ -80,25 +80,26 @@ window.addEventListener("scroll", (event) => {
        $(".pl1").trigger("play");
        $("#img2").show();
        $(".sidebar a").css("color","white");
-       $("#a4").css("opacity","1");
-       $("#a2").css("opacity","0.5");
+    //   $("#a4").css("opacity","1");
+    //   $("#a2").css("opacity","0.5");
+
        $(".soc").trigger("play");
        $(".soc2").trigger("play");
     }
 
     if(scroll<3390 & scroll>1900){
       $("#v3").hide();
-      $("#a3").css("opacity","0.5");
-      $("#a4").css("opacity","1");
+    //  $("#a3").css("opacity","0.5");
+    //  $("#a4").css("opacity","1");
     }
 
-    //Laundry shot plays, section 1 hideas
+    //Laundry shot plays, section 1 hides
 
     if(scroll>3390){
          $("#v3").show();
          $("#v3").trigger("play");
          $("#v3").autoplay = true;
-         $("#a4").css("opacity","0.5");     
+      //   $("#a4").css("opacity","0.5");     
     }
 
     //laundry
@@ -107,25 +108,25 @@ window.addEventListener("scroll", (event) => {
         $("#v3").show();
         $("#v3").trigger("play");
         $("#v3").autoplay = true;
-        $("#a3").css("opacity","1");
-        $("#a33").css("opacity","0.5");
+    //    $("#a3").css("opacity","1");
+    //    $("#a33").css("opacity","0.5");
     }
 
     if(scroll>4200){
       //3pm and cleaning video show
-       $("#a3").css("opacity","0.5");
-       $("#a33").css("opacity","1");
+    //   $("#a3").css("opacity","0.5");
+    //   $("#a33").css("opacity","1");
        $("#v3").hide();
        $("#v4").show();
        $("#v4").trigger("play");
        $("#v4").autoplay = true;
           
     }
-     //Show section 4
+     //Show section 4 Alarms
 
      if(scroll>4900){
-      $("#a33").css("opacity","0.5");
-      $("#a6").css("opacity","1");
+   //   $("#a33").css("opacity","0.5");
+   //   $("#a6").css("opacity","1");
       $("#v4").hide();
       $(".section4").show();
           
@@ -134,16 +135,16 @@ window.addEventListener("scroll", (event) => {
     if(scroll>5600){
 
    
-      $("#a7").css("opacity","1");
+  //    $("#a7").css("opacity","1");
       $(".pl3").trigger("pause");
-      $("#a6").css("opacity","0.5");
+   //   $("#a6").css("opacity","0.5");
  
     }
      //Hides sidebar for final section
     if(scroll>6000){
 
-    $("#a3").css("opacity","0.5");
-    $("#a6").css("opacity","1");
+ //   $("#a3").css("opacity","0.5");
+ //   $("#a6").css("opacity","1");
     $(".sidebar").hide();
  
     }
@@ -152,6 +153,119 @@ window.addEventListener("scroll", (event) => {
         $(".sidebar").show();
 
 
+//POSITION TRAACKINGG (TIME)
+
+//Food (7:00am)
+if(scroll<800){
+    $("#a1").css("opacity","1");
+}
+else {
+   $("#a1").css("opacity","0.5");
+}
+
+//Coffee (8:00am)
+if(scroll>800 & scroll<1900){
+  $("#a2").css("opacity","1");
+}
+else {
+   $("#a2").css("opacity","0.5");
+}
+
+//Scroll (11:00am)
+if(scroll>1900 & scroll<3390){
+  $("#a4").css("opacity","1");
+}
+else {
+   $("#a4").css("opacity","0.5");
+}
+
+//Laundry (2pm)
+if(scroll>3390 & scroll<4200){
+  $("#a3").css("opacity","1");
+}
+
+else {
+   $("#a3").css("opacity","0.5");
+}
+
+//cleaning (3pm)
+if(scroll>4200 & scroll<4900){
+  $("#a33").css("opacity","1");
+}
+
+else {
+   $("#a33").css("opacity","0.5");
+}
+
+//Alarms (4pm)
+if(scroll>4900 & scroll<5800){
+  $("#a6").css("opacity","1");
+}
+
+else {
+   $("#a6").css("opacity","0.5");
+}
+
+//Badminton (4pm)
+if(scroll>5800){
+  $("#a7").css("opacity","1");
+}
+
+else {
+   $("#a7").css("opacity","0.5");
+}
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//CLICKING FUNCTIONS
+
+//Scroll to food
+$("#a1").on("click",function(){
+gsap.to(window, {duration: 2, scrollTo: 1});
+});
+
+
+//Scroll to chai
+$("#a2").on("click",function(){
+gsap.to(window, {duration: 2, scrollTo: 801});
+});
+
+//Scroll to Section 1
+$("#a4").on("click",function(){
+gsap.to(window, {duration: 2, scrollTo: 2400});
+});
+
+//Scroll to Laundry
+$("#a3").on("click",function(){
+gsap.to(window, {duration: 2, scrollTo: 3700});
+});
+
+//Scroll to cleaning
+$("#a33").on("click",function(){
+gsap.to(window, {duration: 2, scrollTo: 4501});
+});
+
+//Scroll to Section2 alarm
+$("#a6").on("click",function(){
+gsap.to(window, {duration: 2, scrollTo: 5201});
+});
+
+//Scroll to outdoor time
+$("#a7").on("click",function(){
+gsap.to(window, {duration: 2, scrollTo: 5801});
 });
 
 
@@ -160,4 +274,11 @@ $("video").on("mouseenter",function(){
 $(this).trigger("play");
        $(this).autoplay = true;
 });
+
+
+
+
+
+
+
 
